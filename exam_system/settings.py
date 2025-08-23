72a1f31f94db6d2b61e15e00a8e7c364353263ca
vamshi <vamshikrishna8330@gmail.com>
2025-08-24 03:17:23 +0530

    Update settings and exam_result template

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -29,8 +29,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+EMAIL_HOST_USER = os.environ.get("vamshikrishna8330@gmail.com")
+EMAIL_HOST_PASSWORD = os.environ.get("dafl utcn wcxq ozkm")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # ----------------------------------
