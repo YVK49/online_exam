b72f6b1b8c8c79677a7d41b40c168e553d34a614
vamshi <vamshikrishna8330@gmail.com>
2025-08-23 23:27:35 +0530

    Initial commit

--- exam_system/settings.py
+++ exam_system/settings.py
@@ -108,12 +108,16 @@ WSGI_APPLICATION = "exam_system.wsgi.application"
 # -------------------------
 # Supabase (Postgres)
 # -------------------------
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
+
 
 LOGGING = {
     'version': 1,
